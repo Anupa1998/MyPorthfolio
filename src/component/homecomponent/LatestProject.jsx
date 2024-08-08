abd5dce0e3243f3f9d7e83b6dd2bb065fa4f4f6d
Anupa Gamage <anupakavishka@gmail.com>
2024-08-08 09:28:28 +0530

    Update

--- src/component/homecomponent/LatestProject.jsx
+++ src/component/homecomponent/LatestProject.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import img from "../../assets/test.png"
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import ProjectCard from '../ProjectCard/ProjectCard';
